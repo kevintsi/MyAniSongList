70c412257170565f92c9cbbb21899358f9059f90
Kévin TSI <kevtsiii@gmail.com>
2023-04-21 17:22:51 +0200

    Modèles extrait de la base de données

--- backend/models.py
+++ backend/models.py
@@ -1,16 +1,84 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, BLOB, Date
+# coding: utf-8
+from sqlalchemy import BigInteger, Column, Date, ForeignKey, Integer, LargeBinary, String, Table, Text
 from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
 
-from .database import Base
+
+Base = declarative_base()
+metadata = Base.metadata
 
 
 class Account(Base):
-    __tablename__ = "user"
+    __tablename__ = 'Account'
 
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True)
-    password = Column(String, unique=True)
-    email = Column(String)
-    profil_picture = Column(BLOB)
-    is_manager = Column(Boolean)
-    creation_date = Column(Date)
+    id = Column(BigInteger, primary_key=True)
+    username = Column(String(250), nullable=False, unique=True)
+    email = Column(String(250), nullable=False, unique=True)
+    password = Column(String(250), nullable=False)
+    profilPicture = Column(LargeBinary)
+    isManager = Column(Integer, nullable=False)
+    creationDate = Column(Date)
+
+
+class Anime(Base):
+    __tablename__ = 'Anime'
+
+    id = Column(BigInteger, primary_key=True)
+    name = Column(String(250), nullable=False, unique=True)
+    posterImg = Column(LargeBinary, nullable=False)
+    description = Column(Text, nullable=False)
+
+
+class Author(Base):
+    __tablename__ = 'Author'
+
+    id = Column(BigInteger, primary_key=True)
+    name = Column(String(250), nullable=False, unique=True)
+    posterImg = Column(LargeBinary, nullable=False)
+
+    Music = relationship('Music', secondary='Chante')
+
+
+t_Chante = Table(
+    'Chante', metadata,
+    Column('id_Music', ForeignKey('Music.id'),
+           primary_key=True, nullable=False),
+    Column('id_Author', ForeignKey('Author.id'),
+           primary_key=True, nullable=False, index=True)
+)
+
+
+class Music(Base):
+    __tablename__ = 'Music'
+
+    id = Column(BigInteger, primary_key=True)
+    name = Column(String(250), nullable=False)
+    posterImg = Column(LargeBinary)
+    releaseDate = Column(Date, nullable=False)
+    id_Anime = Column(ForeignKey('Anime.id'), nullable=False, index=True)
+    id_Type = Column(ForeignKey('Type.id'), nullable=False, index=True)
+
+    Anime = relationship('Anime')
+    Type = relationship('Type')
+
+
+class Review(Base):
+    __tablename__ = 'Review'
+
+    id = Column(BigInteger, primary_key=True)
+    noteVisual = Column(Integer, nullable=False)
+    noteMusic = Column(Integer, nullable=False)
+    description = Column(Text)
+    creationDate = Column(Date, nullable=False)
+    id_Music = Column(ForeignKey('Music.id'), nullable=False, index=True)
+    id_Account = Column(ForeignKey('Account.id'), nullable=False, index=True)
+
+    Account = relationship('Account')
+    Music = relationship('Music')
+
+
+class Type(Base):
+    __tablename__ = 'Type'
+
+    id = Column(BigInteger, primary_key=True)
+    typeName = Column(String(250), nullable=False, unique=True)
