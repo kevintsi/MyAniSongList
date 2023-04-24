7cc62b8b36c058111d29413c0678c37829173a51
Kévin TSI <kevtsiii@gmail.com>
2023-04-24 14:03:41 +0200

    Switch to Angular

--- frontend/src/pages/index.js
+++
@@ -1,9 +0,0 @@
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
-
-function HomePage() {
-  return <div>Welcome to Next.js!</div>
-}
-
-export default HomePage
