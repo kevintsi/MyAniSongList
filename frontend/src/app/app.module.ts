23c7fe788fb49b9ace7ed0049d8ec712f722405e
Kévin TSI <kevtsiii@gmail.com>
2023-04-24 15:17:06 +0200

    Init Angular project + some routes added (login, register)

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
