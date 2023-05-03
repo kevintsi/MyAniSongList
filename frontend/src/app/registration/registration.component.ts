e1710a4ec4d8920bdb8216c8e6d0b7e1a33ddcdc
Kévin TSI <kevtsiii@gmail.com>
2023-05-03 21:12:37 +0200

    Registration component ts and html updated

--- frontend/src/app/registration/registration.component.ts
+++ frontend/src/app/registration/registration.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.css']
 })
+
+
 export class RegistrationComponent {
+
+  registrationForm = this.formBuilder.group({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: ""
+  });
+
+  constructor(private formBuilder: FormBuilder) {
+    console.log("In constructor");
+  }
+
+  onSubmit(): void {
+    console.log("onSubmit")
+    console.log(this.registrationForm.value)
+  }
 
 }
