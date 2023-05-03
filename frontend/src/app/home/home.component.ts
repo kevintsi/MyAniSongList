1d548b0c6306163873f208f46586b91685e5aba9
Kévin TSI <kevtsiii@gmail.com>
2023-05-03 21:11:20 +0200

    Home component

---
+++ frontend/src/app/home/home.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  isLoggedIn: boolean = false
+  constructor(private authService: AuthService) { }
+
+  ngOnInit(): void {
+    this.isLoggedIn = this.authService.isLoggedIn()
+  }
+}
