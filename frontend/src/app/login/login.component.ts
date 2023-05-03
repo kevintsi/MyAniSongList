8aeac88ce7aae97b0525315f7e44df8e2fa3573f
Kévin TSI <kevtsiii@gmail.com>
2023-05-03 21:12:01 +0200

    Login component ts and html updated

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from "@angular/forms"
+import { AuthService } from '../_services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +10,27 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  loginForm = this.formBuilder.group({
+    username: '',
+    password: ''
+  })
+  constructor(private formBuilder: FormBuilder, private authService: AuthService, private router: Router) {
+    console.log("In constructor");
+  }
+
+  onSubmit() {
+    console.log("onSubmit")
+    console.log(this.loginForm.value)
+    const { username, password } = this.loginForm.value
+    this.authService.login(String(username), String(password))
+      .subscribe({
+        next: data => {
+          this.authService.setSession(data)
+          this.router.navigate(["/", "home"])
+        },
+        error: err => {
+          console.log(err)
+        }
+      })
+  }
 }
