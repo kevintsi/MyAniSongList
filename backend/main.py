7d4e4c5dad105dda34392b627481373c580b60b0
Kévin TSI <kevtsiii@gmail.com>
2023-05-10 16:02:52 +0200

    Update gitignore, main and utils

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routers import users
+from routers import users, animes
 
 
 app = FastAPI()
@@ -19,3 +19,4 @@ app.add_middleware(
 
 
 app.include_router(users.router)
+app.include_router(animes.router)
