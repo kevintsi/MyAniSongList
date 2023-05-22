b83abb0653ee7cd2c4832873d7ac639428dcba10
Kévin TSI <kevtsiii@gmail.com>
2023-05-22 15:52:37 +0200

    Title for anime list component

--- frontend/src/app/components/anime-list/anime-list.component.ts
+++ frontend/src/app/components/anime-list/anime-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Anime } from '../../models/Anime';
 import { AnimeService } from '../../_services/anime.service';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-anime-list',
@@ -11,8 +12,9 @@ export class AnimeListComponent implements OnInit {
   loading = true
   animes?: Anime[]
 
-  constructor(private service: AnimeService) { }
+  constructor(private service: AnimeService, private title: Title) { }
   ngOnInit(): void {
+    this.title.setTitle(this.title.getTitle() + " - Liste d'animés")
     this.service.get_all().subscribe({
       next: (animes) => {
         console.log("Animes : ", animes)
