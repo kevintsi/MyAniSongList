a2ea59a7e59c05220f3a717a6b18bd1274117cb7
Kévin TSI <kevtsiii@gmail.com>
2023-06-06 17:34:20 +0200

    Update name folder animes -> anime artists -> artist

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,12 +4,12 @@ import { LoginComponent } from './components/login/login.component'
 import { isSignedInGuard, notSignedInGuard } from './guards/authguard.service';
 import { HomeComponent } from './components/home/home.component';
 import { ProfileComponent } from './components/profile/profile.component';
-import { AnimeListComponent } from './components/animes/anime-list/anime-list.component';
+import { AnimeListComponent } from './components/anime/anime-list/anime-list.component';
 import { RegistrationComponent } from './components/registration/registration.component';
 import { ManageComponent } from './components/management/manage/manage.component';
-import { AnimeDetailComponent } from './components/animes/anime-detail/anime-detail.component';
-import { ArtistListComponent } from './components/artists/artist-list/artist-list.component';
-import { ArtistDetailComponent } from './components/artists/artist-detail/artist-detail.component';
+import { AnimeDetailComponent } from './components/anime/anime-detail/anime-detail.component';
+import { ArtistListComponent } from './components/artist/artist-list/artist-list.component';
+import { ArtistDetailComponent } from './components/artist/artist-detail/artist-detail.component';
 
 
 const routes: Routes = [
