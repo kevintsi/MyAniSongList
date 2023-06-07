fb38f212469eeb62d7b902767bea7a8276852b0b
Kévin TSI <kevtsiii@gmail.com>
2023-06-07 13:34:21 +0200

    Search bar WIP

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule } from '@angular/forms'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -40,6 +40,7 @@ import { MusicDetailComponent } from './components/music/music-detail/music-deta
   imports: [
     HttpClientModule,
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
     ReactiveFormsModule,
     BrowserAnimationsModule,
--- frontend/src/app/components/top-bar/top-bar.component.ts
+++ frontend/src/app/components/top-bar/top-bar.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../_services/auth.service';
 import { Router } from '@angular/router';
 import { StorageService } from '../../_services/storage.service';
+import { MusicService } from 'src/app/_services/music.service';
+import { ArtistService } from 'src/app/_services/artist.service';
+import { AnimeService } from 'src/app/_services/anime.service';
+import { Observable, Subject, debounceTime, distinctUntilChanged, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-top-bar',
@@ -12,19 +16,60 @@ import { StorageService } from '../../_services/storage.service';
 
 export class TopBarComponent implements OnInit {
   isMenuOpen: boolean = false;
+  isSearchBarOpen: boolean = false
+  category: string = "animes"
+  result_search?: Observable<any[]>
+  private input_term = new Subject<string>()
 
   constructor(
     private authService: AuthService,
+    private music_service: MusicService,
+    private artist_service: ArtistService,
+    private anime_service: AnimeService,
     private router: Router,
     private storageService: StorageService
   ) { }
 
   ngOnInit(): void {
-    console.log("ngOnInit TopBarComponent")
+    this.input_term.pipe(
+      debounceTime(500),
+      distinctUntilChanged(),
+    ).subscribe((query: string) => {
+      console.log("New search : ", this.category)
+      switch (this.category) {
+        case "animes": {
+          this.result_search = this.anime_service.search(query)
+        }
+          break
+        // case "musics": {
+        //   this.music_service.search(query)
+        // }
+        // break
+        case "artists": {
+          this.result_search = this.artist_service.search(query)
+        }
+          break
+      }
+    })
+  }
+
+  get_value(event: Event): string {
+    return (event.target as HTMLInputElement).value;
+  }
+
+  get_list(term: string) {
+    this.input_term.next(term)
+
   }
 
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
+    this.isSearchBarOpen = false;
+  }
+
+  toggleSearchBar() {
+    this.isSearchBarOpen = !this.isSearchBarOpen;
+    this.isMenuOpen = false;
   }
 
 
