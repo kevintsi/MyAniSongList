0fc4f75e6226d3d244ca1bdf931c21e3f50646a6
Kévin TSI <kevtsiii@gmail.com>
2023-06-22 20:59:46 +0200

    Create and getUserReview by music id added

--- frontend/src/app/_services/review.service.ts
+++ frontend/src/app/_services/review.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../environments/environment';
-import { HttpClient } from '@angular/common/http';
-import { PagedReview } from '../models/Review';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { PagedReview, Review } from '../models/Review';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,11 +10,35 @@ import { PagedReview } from '../models/Review';
 export class ReviewService {
 
   endpoint: string = environment.REST_API_URL
+  reviewAdded: Subject<boolean>
 
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this.reviewAdded = new Subject<boolean>()
+  }
 
   public getAll(id_music: number, page: number = 1, size: number = 10) {
     return this.http.get<PagedReview>(this.endpoint + '/reviews/music/' + id_music + "?page=" + page + "&size=" + size)
   }
+
+  public create(review: any) {
+    const headers = new HttpHeaders().set('Content-Type', 'application/json')
+
+    console.log(review)
+
+    return this.http.post(
+      this.endpoint + '/reviews/add',
+      JSON.stringify(review),
+      {
+        headers: headers,
+      }
+    )
+  }
+
+  public getUserReview(id_music: number) {
+
+    return this.http.get<Review | null>(
+      this.endpoint + '/reviews/user/music/' + id_music)
+  }
+
 }
