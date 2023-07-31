b63d8ac4c9d20aa180e5bd15417436398bc91278
Kévin TSI <kevtsiii@gmail.com>
2023-07-31 14:35:38 +0200

    Add popular and latest music in home

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,15 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { firstValueFrom } from 'rxjs';
+import { MusicService } from 'src/app/_services/music.service';
+import { Music } from 'src/app/models/Music';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  isLoading: boolean = true
+  latestMusics: Music[] = []
+  popularMusics: Music[] = []
 
-  constructor(private title: Title) {
+  constructor(private title: Title, private musicService: MusicService) {
     this.title.setTitle("MyAniSongList - Accueil")
+  }
+
+  ngOnInit() {
+    this.fetchData()
+  }
+
+  async fetchData() {
+    try {
+      this.latestMusics = await this.fetchLatest()
+      this.popularMusics = await this.fetchPopular()
+    } catch (error) {
+      console.log(error)
+    } finally {
+      this.isLoading = false
+    }
+  }
+
+  fetchPopular() {
+    return firstValueFrom(this.musicService.getPopular())
+  }
+
+  fetchLatest() {
+    return firstValueFrom(this.musicService.getLatest())
   }
 
 }
