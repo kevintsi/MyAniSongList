0661508563b64c08b6fc6cc9e124a3fb9b2046ce
Kévin TSI <kevtsiii@gmail.com>
2023-09-18 15:18:46 +0200

    Fix timeout connection

--- backend/db/session.py
+++ backend/db/session.py
@@ -29,7 +29,9 @@ from config import get_settings
 
 
 engine = create_engine(get_settings().database_url,
-                       pool_pre_ping=True, pool_recycle=3600)
+                       pool_pre_ping=True,
+                       pool_recycle=3600,
+                       pool_timeout=30)
 
 
 @lru_cache
