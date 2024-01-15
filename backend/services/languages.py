f86e9c12cd86b344b145bbebb3619ce15f634733
Kévin TSI <kevtsiii@gmail.com>
2024-01-15 11:26:26 +0100

    Add get_current_user dependencies and route for get languages by anime id

--- backend/services/languages.py
+++ backend/services/languages.py
@@ -1,7 +1,7 @@
 from sqlalchemy.orm import Session
 from fastapi import Depends, HTTPException
 from db.schemas.languages import LanguageCreate, LanguageUpdate
-from db.models import Language
+from db.models import AnimeTranslation, Language, User
 from .base import BaseService
 from db.session import get_session
 from sqlalchemy import exc
@@ -11,19 +11,46 @@ class LanguageService(BaseService[Language, LanguageCreate, LanguageUpdate]):
     def __init__(self, db_session: Session):
         super(LanguageService, self).__init__(Language, db_session)
 
-    def create(self, obj: LanguageCreate):
-        db_obj: Language = self.model(**obj.dict())
-        print(f"In BaseService : before {type(obj)} and after {type(db_obj)}")
-        self.db_session.add(db_obj)
-        try:
+    def create(self, obj: LanguageCreate, current_user: User):
+        if current_user.is_manager:
+            db_obj: Language = self.model(**obj.dict())
+            print(
+                f"In BaseService : before {type(obj)} and after {type(db_obj)}")
+            self.db_session.add(db_obj)
+            try:
+                self.db_session.commit()
+            except exc.IntegrityError as e:
+                self.db_session.rollback()
+                if "Duplicate entry" in str(e):
+                    raise HTTPException(
+                        status_code=409, detail="Conflict Error")
+                else:
+                    raise e
+            return db_obj
+        else:
+            raise HTTPException(status_code=401, detail="Forbidden")
+
+    def update(self, id: int, obj: LanguageUpdate, current_user: User):
+        if current_user.is_manager:
+            db_obj = self.db_session.get(Language, id)
+            print(f"Update : {db_obj}")
+            for column, value in obj.dict(exclude_unset=True).items():
+                setattr(db_obj, column, value)
             self.db_session.commit()
-        except exc.IntegrityError as e:
-            self.db_session.rollback()
-            if "Duplicate entry" in str(e):
-                raise HTTPException(status_code=409, detail="Conflict Error")
-            else:
-                raise e
-        return db_obj
+            return db_obj
+        else:
+            raise HTTPException(status_code=401, detail="Forbidden")
+
+    def delete(self, id: int, current_user: User):
+        if current_user.is_manager:
+            db_obj = self.db_session.query(self.model).get(id)
+            self.db_session.delete(db_obj)
+            self.db_session.commit()
+        else:
+            raise HTTPException(status_code=401, detail="Forbidden")
+
+    def get_languages_by_anime(self, id):
+        return self.db_session.query(Language).join(Language.anime_translations).where(AnimeTranslation.id_anime == id).all()
 
 
 def get_service(db_session: Session = Depends(get_session)) -> LanguageService:
