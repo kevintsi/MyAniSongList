1d1bff4c60913059c0b75b876880a6d5b1e2b61e
Kévin TSI <kevtsiii@gmail.com>
2024-02-05 13:28:33 +0100

    Test for favorites done

---
+++ backend/tests/test_favorites.py
@@ -0,0 +1,53 @@
+from datetime import datetime
+import os
+
+import pytest
+from app.db.schemas.languages import LanguageCreate
+from app.db.schemas.types import TypeCreate
+from app.db.schemas.animes import AnimeCreate
+from app.db.schemas.authors import AuthorCreate
+from app.db.schemas.musics import MusicCreate
+
+
+@pytest.mark.usefixtures("setUp")
+class TestFavorites():
+    ENDPOINT_BASE = "/favorites"
+
+    def test_add_favorite(self, test_app_with_db, get_token_not_manager, get_token_manager):
+        id_music = 1
+        response_post = test_app_with_db.post(f"{self.ENDPOINT_BASE}/{id_music}", headers={
+            "Authorization": f"Bearer {get_token_not_manager}"
+        })
+        assert response_post.status_code == 201
+
+        response_get = test_app_with_db.get(f"{self.ENDPOINT_BASE}/all", headers={
+            "Authorization": f"Bearer {get_token_not_manager}"
+        })
+        assert response_get.status_code == 200
+        assert response_get.json()[0]["id"] == id_music
+
+    def test_user_favorites(self, test_app_with_db, get_token_not_manager):
+        response_get = test_app_with_db.get(f"{self.ENDPOINT_BASE}/all", headers={
+            "Authorization": f"Bearer {get_token_not_manager}"
+        })
+
+        assert response_get.status_code == 200
+        assert type(response_get.json()) is list
+
+    def test_other_user_favorites(self, test_app_with_db):
+        response_get = test_app_with_db.get(f"{self.ENDPOINT_BASE}/users/1")
+        assert response_get.status_code == 200
+        assert len(response_get.json()) == 0
+
+    def test_delete_favorite(self, test_app_with_db, get_token_not_manager):
+        response_delete = test_app_with_db.delete(f"{self.ENDPOINT_BASE}/1", headers={
+            "Authorization": f"Bearer {get_token_not_manager}"
+        })
+
+        assert response_delete.status_code == 200
+
+        response_get = test_app_with_db.get(f"{self.ENDPOINT_BASE}/all", headers={
+            "Authorization": f"Bearer {get_token_not_manager}"
+        })
+        assert response_get.status_code == 200
+        assert len(response_get.json()) == 0
