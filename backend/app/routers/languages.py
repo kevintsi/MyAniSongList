7127129f1024b84638ce33904f33e6be69ba2ccf
Kévin TSI <kevtsiii@gmail.com>
2024-02-06 15:20:39 +0100

    Add comments for languages routes

--- backend/app/routers/languages.py
+++ backend/app/routers/languages.py
@@ -1,3 +1,4 @@
+from typing import Annotated
 from fastapi import (
     APIRouter,
     Depends,
@@ -16,33 +17,84 @@ router = APIRouter(
 
 @router.get("/all", response_model=list[Language])
 async def get_all(
-    service: LanguageService = Depends(get_service)
-):
+    service: Annotated[LanguageService, Depends(get_service)]
+) -> list[Language]:
+    """
+    **Route to get all supported languages**
+
+    **Args:**
+
+        service (Annotated[LanguageService, Depends]): Language service
+
+    **Returns:**
+
+        list[Language]: All supported languages
+    """
     return service.list().all()
 
 
 @router.get("/{id}", response_model=Language)
 async def get(
     id: int,
-    service: LanguageService = Depends(get_service)
-):
+    service: Annotated[LanguageService, Depends(get_service)]
+) -> Language:
+    """
+
+    **Route to get language by id**
+
+    **Args:**
+
+        id (int): Language id
+        service (Annotated[LanguageService, Depends]): Language service
+
+    **Returns:**
+
+        Language: Language retrieved
+    """
     return service.get(id)
 
 
 @router.get("/animes/{id}", response_model=list[Language])
 async def get_languages_by_anime(
     id: int,
-    service: LanguageService = Depends(get_service)
-):
+    service: Annotated[LanguageService, Depends(get_service)]
+) -> list[Language]:
+    """
+
+    **Route to get supported languages by an anime**
+
+    **Args:**
+
+        id (int): Anime id
+        service (Annotated[LanguageService, Depends]): Language service
+
+    **Returns:**
+
+        list[Language]: List of supported languages
+    """
     return service.get_languages_by_anime(id)
 
 
 @router.post("/add", response_model=Language, status_code=status.HTTP_201_CREATED)
 async def add(
     language: LanguageCreate,
-    service: LanguageService = Depends(get_service),
-    current_user: User = Depends(get_current_user)
-):
+    service: Annotated[LanguageService, Depends(get_service)],
+    current_user: Annotated[User, Depends(get_current_user)]
+) -> Language:
+    """
+
+    **Route to create a new supported language**
+
+    **Args:**
+
+        language (LanguageCreate): Language create schema
+        service (Annotated[LanguageService, Depends): Language service
+        current_user (Annotated[User, Depends): Get user using the token in the header
+
+    **Returns:**
+
+        Language: Created language
+    """
     return service.create(language, current_user)
 
 
@@ -50,16 +102,42 @@ async def add(
 async def update(
     id: int,
     language: LanguageUpdate,
-    service: LanguageService = Depends(get_service),
-    current_user: User = Depends(get_current_user)
-):
+    service: Annotated[LanguageService, Depends(get_service)],
+    current_user: Annotated[User, Depends(get_current_user)]
+) -> Language:
+    """
+
+    **Route to update a supported language**
+
+    **Args:**
+
+        id (int): Language id
+        language (LanguageUpdate): Language update schema
+        service (Annotated[LanguageService, Depends): Language service
+        current_user (Annotated[User,Depends): Get user using the token in the header
+
+    **Returns:**
+
+        Language: Language updated
+    """
     return service.update(id, language, current_user)
 
 
 @router.delete("/delete/{id}")
 async def delete(
     id: int,
-    service: LanguageService = Depends(get_service),
-    current_user: User = Depends(get_current_user)
+    service: Annotated[LanguageService, Depends(get_service)],
+    current_user: Annotated[User, Depends(get_current_user)]
 ):
+    """
+
+    **Route to delete a supported language**
+
+    **Args:**
+
+        id (int): Language id
+        service (Annotated[LanguageService, Depends): Language service
+        current_user (Annotated[User, Depends): Get user using the token in the header
+
+    """
     return service.delete(id, current_user)
