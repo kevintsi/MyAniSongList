b2e045a9e6b24b2f514f24fa82cf4d90c8d0fdb7
Kévin TSI <kevtsiii@gmail.com>
2024-02-12 16:52:27 +0100

    Update and add methods

---
+++ frontend/src/app/_services/language.service.ts
@@ -0,0 +1,46 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Language } from '../models/Language';
+import { Anime } from '../models/Anime';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LanguageService {
+  endpoint: string = environment.REST_API_URL
+
+  constructor(private http: HttpClient) { }
+
+  public getAll() {
+    return this.http.get<Language[]>(this.endpoint + '/languages/all')
+  }
+
+  public get(id: number) {
+    return this.http.get<Language>(this.endpoint + '/languages/' + id)
+  }
+
+  public getSupportedLanguagesByAnime(anime: Anime) {
+    return this.http.get<Language[]>(this.endpoint + '/languages/animes' + anime.id)
+  }
+
+  public add(lang: Language) {
+    const headers = new HttpHeaders().set('Content-Type', 'application/json')
+    return this.http.post<Language>(this.endpoint + "/languages/add", JSON.stringify(lang), {
+      headers: headers
+    })
+  }
+
+  public update(id: Number, updatedLang: Language) {
+    const headers = new HttpHeaders().set('Content-Type', 'application/json')
+    return this.http.put<Language>(this.endpoint + "/languages/update/" + id, JSON.stringify(updatedLang),
+      {
+        headers: headers
+      })
+  }
+
+  public delete(lang: Language) {
+    return this.http.delete(this.endpoint + "/languages/delete/" + lang.id)
+  }
+
+}
