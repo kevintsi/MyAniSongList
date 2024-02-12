bcd7386023bff1d6e567af07ee36ef378d66c069
Kévin TSI <kevtsiii@gmail.com>
2024-02-12 16:52:54 +0100

    Update TypeService and add methods

--- frontend/src/app/_services/type.service.ts
+++ frontend/src/app/_services/type.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Type } from '../models/Type';
 
 @Injectable({
@@ -12,12 +12,37 @@ export class TypeService {
 
   constructor(private http: HttpClient) { }
 
-  public getAll() {
-    return this.http.get<Type[]>(this.endpoint + '/types/all')
+  public getAll(lang: String = "fr") {
+    return this.http.get<Type[]>(this.endpoint + '/types/all?lang=' + lang)
   }
 
-  public get(id: number) {
-    return this.http.get<Type>(this.endpoint + '/types/' + id)
+  public get(id: number, lang: String = "fr") {
+    return this.http.get<Type>(this.endpoint + '/types/' + id + "?lang=" + lang)
+  }
+
+  public add(type: Type) {
+    const headers = new HttpHeaders().set('Content-Type', 'application/json')
+    return this.http.post<Type>(this.endpoint + "/types/add", JSON.stringify(type), {
+      headers: headers
+    })
+  }
+
+  public addTranslation(type: Type, lang: String = "fr") {
+    const headers = new HttpHeaders().set('Content-Type', 'application/json')
+    return this.http.post<Type>(this.endpoint + "/types/" + type.id + "/add_translation?lang=" + lang, JSON.stringify(type), {
+      headers: headers
+    })
+  }
+
+  public updateTranslation(id: number, updatedType: Type, lang: String = "fr") {
+    const headers = new HttpHeaders().set('Content-Type', 'application/json')
+    return this.http.put<Type>(this.endpoint + "/types/" + id + "/update_translation?lang=" + lang, JSON.stringify(updatedType), {
+      headers: headers
+    })
+  }
+
+  public delete(type: Type) {
+    return this.http.delete(this.endpoint + "/types/delete/" + type.id)
   }
 
 }
