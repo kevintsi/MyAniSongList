47f3fb6e76276cb1106361023b80607f47018af2
Kévin TSI <kevtsiii@gmail.com>
2024-02-15 17:34:59 +0100

    Update language service

--- frontend/src/app/_services/language.service.ts
+++ frontend/src/app/_services/language.service.ts
@@ -21,6 +21,10 @@ export class LanguageService {
     return this.http.get<Language>(this.endpoint + '/languages/' + id)
   }
 
+  public getLanguage() {
+    return JSON.parse(String(localStorage.getItem("lang")))["id"]
+  }
+
   public getSupportedLanguagesByAnime(anime: Anime) {
     return this.http.get<Language[]>(this.endpoint + '/languages/animes/' + anime.id)
   }
