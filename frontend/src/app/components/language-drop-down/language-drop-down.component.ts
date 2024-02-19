a12ae7c9847f8b7ecc609edaa97e5d906feb9d62
Kévin TSI <kevtsiii@gmail.com>
2024-02-19 13:36:58 +0100

    Fix language dropdown default value

--- frontend/src/app/components/language-drop-down/language-drop-down.component.ts
+++ frontend/src/app/components/language-drop-down/language-drop-down.component.ts
@@ -1,17 +1,24 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-language-drop-down',
   templateUrl: './language-drop-down.component.html',
   styleUrls: ['./language-drop-down.component.css']
 })
-export class LanguageDropDownComponent {
+export class LanguageDropDownComponent implements OnInit {
+
   @Input() languages: any[] = [];
   @Output() languageSelected = new EventEmitter<any>();
 
-  selectedLanguage: any = localStorage.getItem("lang") ?
-    JSON.parse(String(localStorage.getItem("lang"))) : JSON.stringify({ id: "fr", code: "Français" })
+  selectedLanguage: any
   isDropdownOpen = false;
+
+  ngOnInit(): void {
+    this.selectedLanguage = localStorage.getItem("lang") ?
+      JSON.parse(String(localStorage.getItem("lang"))) : { id: "fr", code: "Français" }
+
+    localStorage.setItem("lang", JSON.stringify(this.selectedLanguage))
+  }
 
   toggleDropdown() {
     this.isDropdownOpen = !this.isDropdownOpen;
