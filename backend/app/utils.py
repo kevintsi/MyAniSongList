3a9e333ca30130fcbeb83d6823286ea3d9dd2956
Kévin TSI <kevtsiii@gmail.com>
2025-11-19 01:06:18 +0100

    Fix: Password hashing

--- backend/app/utils.py
+++ backend/app/utils.py
@@ -30,12 +30,12 @@ def authenticate_user(db_session: Session, email: str, password: str):
 
 
 def verify_password(plain_password, hashed_password):
-    return bcrypt.checkpw(plain_password, hashed_password)
+    return bcrypt.checkpw(plain_password.encode("utf-8"), hashed_password)
 
 
 def get_password_hash(password):
     salt = bcrypt.gensalt()
-    return bcrypt.hashpw(password,salt)
+    return bcrypt.hashpw(password.encode("utf-8"),salt)
 
 
 def create_access_token(data: dict, expires_delta: timedelta):
